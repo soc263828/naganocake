0a55dc1b72996f1457a5947869fad1024aa7b41f
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-09-21 07:30:44 +0000

    about-page

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Rails.application.routes.draw do
   end
   namespace :public do
     get 'homes/top'
+    get 'homes/about'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  namespace :public do
