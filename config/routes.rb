02a7ac1f32e2db4dc5a44f486388983bbb80e52e
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-09-27 06:26:15 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
   namespace :admin do
     get 'orders/show'
-  end
-  namespace :admin do
+
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
-  end
-  namespace :admin do
+
     get 'items/index'
     get 'items/new'
     get 'items/create'
     get 'items/show'
     get 'items/edit'
     get 'items/update'
-  end
-  namespace :admin do
+
     get 'homes/top'
-  end
-  namespace :admin do
+
     get 'sessions/new'
     get 'sessions/create'
     get 'sessions/destroy'
@@ -31,35 +36,29 @@ Rails.application.routes.draw do
     get 'orders/create'
     get 'orders/index'
     get 'orders/show'
-  end
-  namespace :public do
+
     get 'cart_items/index'
     get 'cart_items/update'
     get 'cart_items/destroy'
     get 'cart_items/destroy_all'
     get 'cart_items/create'
-  end
-  namespace :public do
+
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
     get 'customers/quit'
     get 'customers/out'
-  end
-  namespace :public do
+
     get 'sessions/new'
     get 'sessions/create'
     get 'sessions/destroy'
-  end
-  namespace :public do
+
     get 'registrations/new'
     get 'registrations/create'
-  end
-  namespace :public do
+
     get 'items/index'
     get 'items/show'
-  end
-  namespace :public do
+
     get 'homes/top'
     get 'homes/about'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_20_072250) do
+ActiveRecord::Schema.define(version: 2023_09_27_050633) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "email", null: false
-    t.string "encryled_password", null: false
-    t.string "post_code", null: false
+    t.string "postal_code", null: false
     t.string "address", null: false
-    t.string "telephone_namber", null: false
+    t.string "telephone_number", null: false
     t.boolean "is_deleted", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "items", force: :cascade do |t|
@@ -41,11 +59,19 @@ ActiveRecord::Schema.define(version: 2023_09_20_072250) do
   end
 
   create_table "order_details", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "postage", null: false
+    t.string "address", null: false
+    t.string "postal_code", null: false
+    t.string "name", null: false
+    t.integer "payment_method", null: false
+    t.integer "charge", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
