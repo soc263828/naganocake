c312d07bf0916869d0ddc987d6259b2df8ee5495
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-10-10 13:20:33 +0000

    order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def log
@@ -12,8 +13,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
   end
+
+  def confirm
+  @order = Order.new(order_params)
+  @address = Address.find(params[:order][:address_id])
+  @order.postal_code = current_customer.postal_code
+  @order.address = current_customer.address
+  @order.name = current_customer.first_name + current_customer.last_name
+end
+
+private
+def order_params
+  params.require(:order).permit(:payment_method)
+end
+
 end
