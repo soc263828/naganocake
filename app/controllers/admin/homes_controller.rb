04ad057b0c31102fed06d6cfeb4cea3c8b3d7591
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-10-24 02:33:52 +0000

    admin/top

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,12 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.all
+    
   end
+
+  private
+def order_params
+  params.require(:order).permit(:payment_method, :postage, :charge, :postal_code, :address, :name, :order_id, :item_id, :cart_item, :amount)
+end
+
 end
